685ed3dce5ced0cd28c508d71c5e82e05c24fa2a
bulej93 <bulej93@gmail.com>
2022-04-16 19:41:57 +0300

    setting up deposits and rewards withdrawal

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -11,11 +11,15 @@ contract Bank {
     DaiToken public daiToken;
     uint deployTime;
     uint rewardTime;
+    uint totalRewards;
     uint pool1;
     uint pool2;
     uint pool3;
+    uint totalStaked;
+    uint stakers;
+    uint pool3Stakers;
 
-    
+    mapping(address => uint) public balances;
 
     constructor(DaiToken _daiToken) {
         name = 'Bank';
@@ -23,11 +27,86 @@ contract Bank {
         daiToken = _daiToken;
         deployTime = block.timestamp;
         rewardTime = 3 days;
+        
     }
 
     function depositRewards(uint _amount) public {
         require(msg.sender == admin);
         daiToken.transferFrom(admin, address(this), _amount);
+        totalRewards = daiToken.balanceOf(address(this));
+        pool1 = daiToken.balanceOf(address(this)) / 100 * 20;
+        pool2 = daiToken.balanceOf(address(this)) / 100 * 30;
+        pool3 = daiToken.balanceOf(address(this)) / 100 * 50;
+    }
+
+    function stakeTokens(uint _amount) public {
+        require(block.timestamp < deployTime + rewardTime, 'sorry time has passed for deposits');
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+        balances[msg.sender] = balances[msg.sender] +  _amount;
+        totalStaked = totalStaked + _amount;
+        stakers ++;
+    }
+
+    function unStakeTokens() public {
+        require(block.timestamp > rewardTime * 2 + deployTime);
+        require(balances[msg.sender] > 0, 'balances need to be more than 0');
+        //days pool 6
+        if (block.timestamp > rewardTime * 2 + deployTime ) {
+            uint _earnedReward = balances[msg.sender] * pool1 / totalStaked;
+            daiToken.transfer(msg.sender, balances[msg.sender] + _earnedReward);
+            console.log('reward at pool 1 %s', _earnedReward );
+            if(block.timestamp < rewardTime * 3 + deployTime){
+                stakers --; 
+            } else {
+                stakers = stakers;
+            }
+        } 
+         console.log('stakers %s at end of pool 1', stakers);
+        //9 days
+        if(block.timestamp > rewardTime * 3 + deployTime ){
+             console.log('stakers at of pool 2  %s', stakers);
+            if(stakers > 1) {
+                uint _earnedRewardP2 = balances[msg.sender] * pool2 / totalStaked;
+                daiToken.transfer(msg.sender, _earnedRewardP2);
+                console.log('reward at pool 2 not alone %s', _earnedRewardP2);
+                
+            } else {
+                uint _earnedRewardP2 = pool2;
+                daiToken.transfer(msg.sender, _earnedRewardP2);
+                console.log('reward at pool 2 alone %s', _earnedRewardP2);
+            }
+        }
+        
+          console.log('pool3 stakers %s', stakers);
+
+          //12 days
+
+       
+        if (block.timestamp > rewardTime * 4 + deployTime) {
+            console.log('stakers %s at pool 3', stakers);
+            if(stakers > 1){
+                uint _earnedRewardP3 = balances[msg.sender] * pool3 / totalStaked;
+                console.log('reward at pool 3 not alone %s', _earnedRewardP3);
+                daiToken.transfer(msg.sender, _earnedRewardP3 );
+
+             
+            } else {
+                uint _earnedRewardP3 = pool3;
+                console.log('reward at pool 3 alone %s', _earnedRewardP3);
+                daiToken.transfer(msg.sender, _earnedRewardP3);
+            }
+        }
+         balances[msg.sender] =  balances[msg.sender] - balances[msg.sender];
+         
+         
+    }
+
+    function withdrawByBank() public {
+        require(msg.sender == admin);
+        require(daiToken.balanceOf(address(this)) != 0, 'sorry contract is empty');
+        if(stakers == 0 && block.timestamp > rewardTime * 4 + deployTime){
+            daiToken.transfer(admin, daiToken.balanceOf(address(this)));
+        }
     }
 
 }
--- test/test.js
+++ test/test.js
@@ -18,8 +18,8 @@ describe("Bank contract", function () {
          DaiToken = await ethers.getContractFactory('DaiToken');
          daitoken = await DaiToken.deploy()
          
-         Bank = await ethers.getContractFactory("Bank", await daitoken.approve(Bank.address, 100));
-         bank = await Bank.connect(owner).deploy(daitoken.address, 100)
+         Bank = await ethers.getContractFactory("Bank");
+         bank = await Bank.connect(owner).deploy(daitoken.address)
          
          
         //  bank = await Bank.deploy(daitoken.address, 100);
@@ -36,11 +36,43 @@ describe("Bank contract", function () {
       expect(await bank.name()).to.equal('Bank')
       const ownerBalance = await daitoken.balanceOf(owner.address)
       //expect(await daitoken.totalSupply()).to.equal(ownerBalance)
-
-
       const bl = await daitoken.balanceOf(bank.address)
 
-      console.log(Bank)
+      // console.log(Bank)
     });
+
+    it('deposit rewards', async function () {
+      await daitoken.connect(owner).approve(bank.address, 1500)
+      await bank.connect(owner).depositRewards(1500)
+      expect(await daitoken.balanceOf(bank.address)).to.equal(1500)
+    })
+
+    it('should deposit staking tokens', async function () {
+      await daitoken.connect(owner).approve(bank.address, 100)
+      await bank.connect(owner).stakeTokens(100)
+      await daitoken.connect(owner).transfer(wallet1.address, 1000)
+      await daitoken.connect(wallet1).approve(bank.address, 1000)
+      await bank.connect(wallet1).stakeTokens(500)
+      expect(await daitoken.balanceOf(bank.address)).to.equal(600)
+    })
+
+    it('unstakes tokens', async function () {
+      await daitoken.connect(owner).approve(bank.address, 150000)
+      await bank.connect(owner).depositRewards(150000)
+      await daitoken.connect(owner).approve(bank.address, 100)
+      await bank.connect(owner).stakeTokens(100)
+      await daitoken.connect(owner).transfer(wallet1.address, 1000)
+      await daitoken.connect(wallet1).approve(bank.address, 1000)
+      await bank.connect(wallet1).stakeTokens(500)
+      await hre.ethers.provider.send('evm_increaseTime', [16 * 24 * 60 * 60]);
+      await bank.connect(wallet1).unStakeTokens()
+      await hre.ethers.provider.send('evm_increaseTime', [15 * 24 * 60 * 60]);
+      await bank.connect(owner).unStakeTokens()
+      expect(await daitoken.balanceOf(wallet1.address)).to.equal(500)
+    })
+
+    it('allow admin to withdraw', async function () {
+      
+    })
 
   });
